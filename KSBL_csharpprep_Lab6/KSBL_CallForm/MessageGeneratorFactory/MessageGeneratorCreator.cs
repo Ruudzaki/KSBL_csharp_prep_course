365c66702d3f0371ca84356aa6e1a6a893d6dae5
Ruudzaki <56171632+Ruudzaki@users.noreply.github.com>
2020-04-18 20:06:19 +0300

    add old folder

--- KSBL_csharpprep_Lab6/KSBL_CallForm/MessageGeneratorFactory/MessageGeneratorCreator.cs
+++
@@ -1,9 +0,0 @@
-﻿using KSBL_Class_Library.Mobile;
-
-namespace KSBL_SmsWinForms_app.MessageGeneratorFactory
-{
-    public abstract class MessageGeneratorCreator
-    {
-        public abstract MessageGenerator Create(Mobile mobile);
-    }
-}
